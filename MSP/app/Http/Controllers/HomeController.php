2dab175965a124a4f3e6aa6bb3baad1f3f575c59
Farnan130117 <mh.farnan@gmail.com>
2020-08-23 00:45:53 +0600

    Setting up products page with dummy product picture

--- MSP/app/Http/Controllers/HomeController.php
+++ MSP/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+       // $this->middleware('auth'); 
+       // Disable this line means, accessing this home page for all guest users
     }
 
     /**
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products= Product::take(10)->get();
+        return view('home',['products' => $products]);
     }
 }
--- MSP/resources/views/home.blade.php
+++ MSP/resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <h2> Products</h2>
+    <div class="row">
+
+        @foreach($products as $product)
+        <div class="col-4">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <img class="card-img-top" src="{{ asset('default_product.png')}}" alt="Card image cap">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h4 class="card-title">{{$product->name}}</h4>
+                    <p class="card-text">{{$product->description}}</p>
                 </div>
+                 <div class="card-body">
+                    <a href="#" class="card-link">Add to cart</a>
+                   <!-- <a href="#" class="card-link">Another link</a> -->
+            </div>
+            
             </div>
         </div>
+        @endforeach
+
     </div>
+    
 </div>
 @endsection
